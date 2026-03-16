0097cbda8044f644413980b8176c99bc96a4f192
mzehnder11 <malik.zehnder1@gmail.com>
2026-03-16 12:16:45 +0100

    Favicon lokal gespeichert

--- setup_password.js
+++ setup_password.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const crypto = require('crypto');
 
 // Hier das eigene Passwort, welches man festlegen möchte eintragen und dann dieses script mit Node ausführen.
